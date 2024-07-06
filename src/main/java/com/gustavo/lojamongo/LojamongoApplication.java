f52acb0018d5773b08a41c5184fa752d6ca3af70
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-07-06 10:32:54 -0300

    Fisrt Commit

---
+++ src/main/java/com/gustavo/lojamongo/LojamongoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.lojamongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojamongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojamongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavo/lojamongo/LojamongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.lojamongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojamongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
