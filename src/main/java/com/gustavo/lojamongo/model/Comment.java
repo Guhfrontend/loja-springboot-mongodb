4951b8c65b821b2df1454900ffc195a3db0b345b
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-07-08 11:51:13 -0300

    entity user e REST funcionando

---
+++ src/main/java/com/gustavo/lojamongo/model/Comment.java
@@ -0,0 +1,11 @@
+package com.gustavo.lojamongo.model;
+
+import java.util.Date;
+
+public class Comment {
+
+    private String id;
+    private String text;
+    private Date date;
+
+}
---
+++ src/main/java/com/gustavo/lojamongo/model/Post.java
@@ -0,0 +1,12 @@
+package com.gustavo.lojamongo.model;
+
+import java.util.Date;
+
+public class Post {
+
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+
+}
